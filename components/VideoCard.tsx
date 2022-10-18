948379af7e7ac43611655c6c39064eafdd773c29
Davide <myearthlydelight@gmail.com>
2022-10-18 23:23:17 +0200

    implemented get posts api

---
+++ components/VideoCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { NextPage } from "next";
+
+import { Video } from "../types";
+
+interface IProps {
+    post: Video;
+}
+
+const VideoCard: NextPage<IProps> = ({ post }) => {
+    return <div>VideoCard</div>;
+};
+
+export default VideoCard;
