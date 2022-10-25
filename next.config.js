84d2f3cf4e4202f4737f56afd4de3b6e3870fc67
Davide <myearthlydelight@gmail.com>
2022-10-25 19:43:29 +0200

    added profile picture in Navbar and logout button

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ["i.postimg.cc"],
+        domains: ["i.postimg.cc", "lh3.googleusercontent.com"],
     },
 };
 
