563bbc275f77118347e8fc1d65961fa81f4efcca
Davide <myearthlydelight@gmail.com>
2022-11-17 19:20:20 +0100

    added details on sidebar and LikeButton and Comments components

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -10,6 +10,9 @@ import axios from "axios";
 
 import { BASE_URL } from "../../utils";
 import { Video } from "../../types";
+import useAuthStore from "../../store/authStore";
+import LikeButton from "../../components/LikeButton";
+import Comments from "../../components/Comments";
 
 interface IProps {
     postDetails: Video;
@@ -21,6 +24,7 @@ const Detail = ({ postDetails }: IProps) => {
     const [isVideoMuted, setIsVideoMuted] = useState(false);
     const videoRef = useRef<HTMLVideoElement>(null);
     const router = useRouter();
+    const { userProfile } = useAuthStore();
 
     const onVideoClick = () => {
         if (playing) {
@@ -76,6 +80,41 @@ const Detail = ({ postDetails }: IProps) => {
                             <HiVolumeUp className="text-white text-2xl lg:text-4xl" />
                         </button>
                     )}
+                </div>
+            </div>
+            <div className="relative w-[1000px] md:w-[900px] lg:w-[700px]">
+                <div className="lg:mt-20 mt-10">
+                    <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+                        <div className="ml-4 md:w-20 md:h-20 w-16 h-16">
+                            <Link href="/">
+                                <>
+                                    <Image
+                                        className="rounded-full"
+                                        width={62}
+                                        height={62}
+                                        src={post.postedBy.image}
+                                        alt="profile photo"
+                                        layout="responsive"
+                                    />
+                                </>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link href="/">
+                                <div className="mt-3 flex flex-col gap-2">
+                                    <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                                        {post.postedBy.userName} <GoVerified className="test-blue-400 text-md" />
+                                    </p>
+                                    <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                                        {post.postedBy.userName}
+                                    </p>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                    <p className="px-10 text-lg text-gray-600">{post.caption}</p>
+                    <div className="mt-10 px-10">{userProfile && <LikeButton />}</div>
+                    <Comments />
                 </div>
             </div>
         </div>
